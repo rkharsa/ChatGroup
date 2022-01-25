8574174b5809059416f7a170304b2a0c161ebf47
Rani kharsa <rkharsa@outlook.fr>
2022-01-26 00:49:14 +0100

    Initial Commit

---
+++ chatGroup/chatGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatGroupApp.swift
+//  chatGroup
+//
+//  Created by Rani Kharsa on 26/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct chatGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
